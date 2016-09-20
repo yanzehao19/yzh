2c18e67fb69602a9e05d3c5b5d2ec5d5ec54899e
zhyan5726 <zhyan5726@fiberhome.com>
2016-09-20 20:06:55 +0800

    git0101

--- src/yzh_test_git/Yzh.java
+++ src/yzh_test_git/Yzh.java
@@ -6,6 +6,22 @@ public class Yzh {
   private int jk1;
   private  int i;
 
+  public int getJk() {
+    return jk;
+  }
+
+  public void setJk(int jk) {
+    this.jk = jk;
+  }
+
+  public int getJk1() {
+    return jk1;
+  }
+
+  public void setJk1(int jk1) {
+    this.jk1 = jk1;
+  }
+
   public int getI() {
     return i;
   }
